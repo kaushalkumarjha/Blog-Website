536a305d52a7b6946bbefbe4dd5dce92e4da56fb
Kaushal Kumar Jha <kaushaljha1332@gmail.com>
2024-08-09 21:14:02 +0530

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo"
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "../pages/Login";
+import Button from "./Button"
+import PostForm from "./Post-Form/PostForm"
+import PostCard from "./PostCard"
+import AuthLayout from "../components/AuthLayout"
+import Select from "./Select"
+import Input from "./Input"
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Select,
+    Input,
+
+}
